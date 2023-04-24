6562c3bdf0f23ffdf8086f2502dc5174b7e82d53
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-04-24 22:02:36 +0530

    login Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 //import { useState } from 'react'
-import Login from './components/login'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Provider} from "react-redux"
+import Login from "./components/login";
+import Home from "./components/Home";
+import {store} from "./components/Store"
 
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-     <Login/>
+    <Provider store={store}>
+    <Router>
+        <Routes>
+
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+
+          <Route />
+        </Routes>
+      </Router>
+    </Provider>
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,35 +1,50 @@
-
 import { useState } from "react";
+import { login } from "./Store";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 function Login() {
-    const [Code,setCode]=useState("");
+  const navigate = useNavigate();
+  const Dispatch = useDispatch();
+  const [Code, setCode] = useState("");
 
-//const [show,setShow]=useState(false);
-
+  const onEnter = () => {
+    axios.get(`https://hitmyapi.netlify.app/`).then((data) => {
+      console.log(Code,data.data);
+      if (Code == data.data.CODE) {
+        Dispatch(login({ code: Code }));
+        navigate("/Home");
+      } else {
+        alert("NOT");
+      }
+    });
+    
+  };
   return (
     <>
-   
-        <div className="mt-5 py-5">
+      <div className="mt-5 py-5">
         <div className="container-fluid d-grid mt-5 ">
-    
-      
           <div className="row">
-    
-          <label className="col-12 text-light py-5 h2">ENTER YOUR CODE</label>
-          <div>
-              <div className="col-4"></div>
-          <input className="col-4 form-control px-5" type="password" value={Code} onChange={(e)=>setCode(e.target.value)}></input>
-          <div className="col-4"></div>
+            <label className="col-12 text-light py-5 h2">ENTER YOUR CODE</label>
+            <div>
+              <div className="col-5"></div>
+              <input
+                className="col-2 form-control px-5"
+                type="password"
+                value={Code}
+                onChange={(e) => setCode(e.target.value)}
+              ></input>
+              <div className="col-5"></div>
+            </div>
           </div>
-          
-          </div>
-          
-          
         </div>
+        <button className="btn btn-primary mt-5" onClick={onEnter}>
+          SUBMIT
+        </button>
       </div>
-        
-   </>
-  )
+    </>
+  );
 }
 
-export default Login
+export default Login;
