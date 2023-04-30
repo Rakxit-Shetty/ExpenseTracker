ebdd4bd2eb0f6f5e04b5f1dccc09d4b39ef8b4b4
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-04-30 22:32:35 +0530

    fix Home

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -1,16 +1,52 @@
-//import React from 'react'
+import React from 'react'
 
 function AddExpense() {
+const [money,setMoney]=React.useState("");
+
+const[data,setData]=React.useState([]);
+  const onAdd=()=>{
+    if(money!==""){
+      const today = new Date();
+      const dd = today.getDate();
+      const mm = today.getMonth() + 1;
+      const yyyy = today.getFullYear();
+      
+setData([{"Money":money, "Date":dd+"-"+mm+"-"+yyyy},...data]);
+setMoney("");
+    }
+  }
+
   return (
-    <div>
-      <div className="text-light h3">
-        <span>
-          {" "}
-          AddExpense <input type="number" className="form-control" />{" "}
-          <button className="btn btn-success">Submit</button>
-        </span>
-      </div>
+    <>
+   <div className="row">
+    <div className="col-lg-4 h2 text-light">ADD</div>
+    <div className="col-lg-4">
+      <input type="number"
+      value={money}
+      onChange={(e)=>setMoney(e.target.value)}
+      className="form-control">
+      </input>
     </div>
+    <div className="col-lg-4 ">
+     <button className="btn btn-primary" onClick={onAdd}>Click to ADD</button>
+      </div>
+      
+   </div>
+   <div className="row">
+    {
+      data.map((ele,index)=>{
+        return <>
+        <div className="card w-25 m-3" key={index}>
+         Date: {ele && ele.Date}<br></br>
+        Spent:{ele && ele.Money}
+        </div>
+       
+        </>
+      })
+    }
+   </div>
+
+   </>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,16 @@
 //import React from 'react'
-// import { logout } from "./Store";
+ import { logout } from "./Store";
 import { Navigate, 
-  // useNavigate 
+   useNavigate 
 } from "react-router-dom";
 import { useSelector } from "react-redux";
-// import { useDispatch } from "react-redux";
+ import { useDispatch } from "react-redux";
 import AddExpense from "./AddExpense";
 function Home() {
-  // const Dispatch = useDispatch();
-  // const navigate = useNavigate();
+   const Dispatch = useDispatch();
+   const navigate = useNavigate();
   const Auth = useSelector((state) => state.user.isAuth);
-  //const Code = useSelector((state) => state.user.value.code);
+  const Code = useSelector((state) => state.user.value.code);
   console.log(Auth);
 
   if (Auth === false) {
@@ -18,10 +18,10 @@ function Home() {
   }
   return (
     <>
-      {/* <div classNameName="h1 text-light">
+      <div className="h1 text-light">
         <div>Loged in with code {Code}</div>
         <button
-          classNameName="btn btn-lg btn-danger mx-5"
+          className="btn btn-lg btn-danger mx-5"
           onClick={() => {
             Dispatch(logout());
             navigate("/");
@@ -29,33 +29,10 @@ function Home() {
         >
           LOGOUT
         </button>{" "}
-      </div> */}
-      <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            {/* <!-- Navbar Brand--> */}
-            <a className="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
-            {/* <!-- Sidebar Toggle--> */}
-            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
-            {/* <!-- Navbar Search--> */}
-            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div className="input-group">
-                    <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
-                </div>
-            </form>
-            {/* <!-- Navbar--> */}
-            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw"></i></a>
-                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a className="dropdown-item" href="#!">Settings</a></li>
-                        <li><a className="dropdown-item" href="#!">Activity Log</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" href="#!">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+      </div>
+      <div className="row mt-5">
       <AddExpense />
+      </div>
 
     </>
   );
